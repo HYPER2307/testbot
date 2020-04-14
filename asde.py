8f044737bf0a78082aae93e63bf80977bdd63b54
HYPER2307 <63514441+HYPER2307@users.noreply.github.com>
2020-04-14 19:06:37 +0300

    Update asde.py

--- asde.py
+++ asde.py
@@ -10,9 +10,9 @@ def welcome(message):
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-	if api_response_error.NotFoundError:
-		print("Ваш пошуковий запит не знайдено")
 	observation = owm.weather_at_place(message.text)
+	якщо  api_response_error . NotFoundError :
+		print ( "Ваш пошуковий запис не знайдено" )
 	w = observation.get_weather()
 	temp = w.get_temperature('celsius')["temp"]
 	wind = w.get_wind()["speed"]
